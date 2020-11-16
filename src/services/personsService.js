c9ba9795557e31146d5587d6e50167b003e7ce72
michael <michaelmckn4650@gmail.com>
2020-11-16 15:08:59 +0000

    update URL

--- src/services/personsService.js
+++ src/services/personsService.js
@@ -2,10 +2,11 @@ import axios from 'axios'
 
 // This module handles methods for communicating with the server.
 
-const baseUrl = 'https://cryptic-fortress-05104.herokuapp.com/persons/api'
+const baseUrl = '/persons/api'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
+  
     return request.then(response => response.data)
 }
 
